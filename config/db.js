3689ee2ec6fb5481803a4e70f757d3409c700bc3
baljinder0630 <baljindersingh41256@gmail.com>
2024-09-19 01:47:43 +0530

    Connected with db

--- config/db.js
+++ config/db.js
@@ -3,12 +3,11 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
 
-        mongoose.set('strictQuery', false)
-        mongoose.connect(process.env.MONGODB_URI)
+        await mongoose.connect(process.env.MONGODB_URI)
         console.log("Mongo connected");
 
     } catch (error) {
-        console.error(error);
+        console.error(error.message);
         process.exit(1);
     }
 }
---
+++ models/user.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: string
+    },
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    hash: {
+        type: String
+    }
+}, { timestamps: true })
+
+const User = mongoose.model('User', userSchema)
+export default User
